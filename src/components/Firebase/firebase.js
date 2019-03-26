3f2be1d4cb925bebebe278c9b953fdcc1dc1feaa
aliu <adam.liu@tw.synaptics.com>
2019-03-26 18:19:10 +0800

    Support role-based authorization

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -29,16 +29,51 @@ class Firebase {
     doSignInWithEmailAndPassword = (email, password) =>
       this.auth.signInWithEmailAndPassword(email, password);
     
-    doSignOut = () => {
-      this.auth.signOut();
-    }
-
+    doSignOut = () => this.auth.signOut();
+    
     doPasswordReset = (email) => 
       this.auth.sendPasswordResetEmail(email);
 
+    doSendEmailVerification = () =>
+      this.auth.currentUser.sendEmailVerification({
+        url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT,
+      });
+      
     doPasswordUpdate = (password) =>  
       this.auth.currentUser.updatePassword(password);
-      
+    
+    // *** Merge Auth and DB User API *** //
+
+  onAuthUserListener = (next, fallback) =>
+    this.auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        this.user(authUser.uid)
+          .once('value')
+          .then(snapshot => {
+            const dbUser = snapshot.val();
+
+            console.log(dbUser);
+            // default empty roles
+            if (!dbUser.roles) {
+              dbUser.roles = [];
+            }
+
+            // merge auth and db user
+            authUser = {
+              uid: authUser.uid,
+              email: authUser.email,
+              emailVerified: authUser.emailVerified,
+              providerData: authUser.providerData,
+              ...dbUser,
+            };
+
+            next(authUser);
+          });
+      } else {
+        fallback();
+      }
+    });
+    
     // *** User API ***
     user = uid => this.db.ref(`users/${uid}`);
 
