d810ef37249abe4c70dafdfec6467b04e2b6d211
aliu <adam.liu@tw.synaptics.com>
2019-03-26 11:20:35 +0800

    Remove config stuff in github

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,13 +1,13 @@
-import app from '@firebase/app';
-import '@firebase/auth';
+import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyBq7JhfCYlAEKY97nkfda3pIOYXStNaLYc",
-    authDomain: "shincar-game-base.firebaseapp.com",
-    databaseURL: "https://shincar-game-base.firebaseio.com",
-    projectId: "shincar-game-base",
-    storageBucket: "shincar-game-base.appspot.com",
-    messagingSenderId: "421163299274"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 class Firebase {
