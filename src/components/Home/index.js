e00b89494fc1a147e017a59f8e52b66638360776
adam <adam.iem92g@gmail.com>
2019-03-27 23:17:25 +0800

    Make landing/home page stylish

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,57 @@
 import React from 'react';
+import { compose } from 'recompose';
+import PropTypes from 'prop-types';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
 
 import { withAuthorization } from '../Session';
 
-const Home = () => (
-  <div>
-      <h1>主畫面</h1>
-      <p>登入的使用者才能看到這頁</p>
-  </div>
-);
+const styles = theme => ({
+  main: {
+    width: 'auto',
+    display: 'block', // Fix IE 11 issue.
+  },
+  paper: {
+    marginTop: theme.spacing.unit,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'left',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+  },
+  typography: {
+    margin: theme.spacing.unit,
+  },
+});
+
+class Home extends React.Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div>
+        <main className={classes.main}>
+          <CssBaseline />
+          <Paper className={classes.paper}>
+          <Typography component="h3" variant="h3" gutterBottom>
+            主畫面
+          </Typography>
+          </Paper>
+        </main>
+      </div>
+    );
+  }
+}
 
 const condition = authUser => !!authUser;
 
-export default withAuthorization(condition)(Home);
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default compose(
+  withAuthorization(condition),
+  withStyles(styles),
+)(Home);
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import { compose } from 'recompose';
+import PropTypes from 'prop-types';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-const Landing = () => (
-  <div>
-      <h1>首頁</h1>
-      <hr />
-  </div>
-);
+const styles = theme => ({
+  main: {
+    width: 'auto',
+    display: 'block', // Fix IE 11 issue.
+  },
+  paper: {
+    marginTop: theme.spacing.unit,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'left',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+  },
+  typography: {
+    margin: theme.spacing.unit,
+  },
+});
 
-export default Landing;
+class Landing extends React.Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div>
+        <main className={classes.main}>
+          <CssBaseline />
+          <Paper className={classes.paper}>
+          <Typography component="h3" variant="h3" gutterBottom>
+            遊戲大廳
+          </Typography>
+          </Paper>
+        </main>
+      </div>
+    );
+  }
+}
+
+Landing.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default compose(
+  withStyles(styles),
+)(Landing);
