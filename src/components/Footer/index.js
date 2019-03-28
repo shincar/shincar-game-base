df44cdeee8619e2dbf0585bbd2c3893e0589a8d9
aliu <adam.liu@tw.synaptics.com>
2019-03-28 12:27:04 +0800

    Make footer stick at bottom. But when content grows, it would be push down.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,25 +1,16 @@
 import React from 'react'
-import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 
-const styles = theme => ({
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    marginTop: theme.spacing.unit * 8,
-    padding: `${theme.spacing.unit * 6}px 0`,
-  },
-});
-
-function Footer(props) {
-  const { classes } = props;
-  
-  return (
-    <footer className={classes.footer}>
-      <Typography variant="h6" align="center" gutterBottom>
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer">
+        <Typography variant="h6" align="center">
         Copyright @ Adam & Alvin's Fun Lab
-      </Typography>
-    </footer>
-  )
+        </Typography>
+      </footer>
+    )
+  }
 }
 
-export default withStyles(styles)(Footer)
+export default Footer
